19a6da0850d7d3cd03ab966785fec06c5598c02c
squashcontroller-hub <squash.controller@gmail.com>
2026-01-31 02:14:33 +0100

    Poprawione obrazki do splashScreena

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mala_atlantyda/Widgets/SplashScreen.dart';
 
-import './auth/LoginUser.dart';
 
 
 
